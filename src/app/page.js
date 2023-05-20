2cb1bf845c93a2f83939319016978a2ee80940ce
Bobby Connolly <thomas.connolly@mcgill.ca>
2023-05-20 12:09:20 -0400

    connected to db

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,26 @@
 import Image from "next/image";
+import { createClient } from "@vercel/postgres";
+
 import styles from "./page.module.css";
 
-export default function Home() {
-  return <main className={styles.main}>Hello</main>;
+export default async function Home() {
+  const client = createClient();
+  await client.connect();
+
+  const { rows } = await client.sql`
+SELECT name, message
+FROM guest_messages`;
+
+  console.log(rows);
+
+  return (
+    <main className={styles.main}>
+      {rows.map((r) => (
+        <span>
+          <span>{r.name}</span>
+          <span>{r.message}</span>
+        </span>
+      ))}
+    </main>
+  );
 }
